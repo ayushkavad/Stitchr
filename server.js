18b809e937b2aa8e983d13817801437f9d54b328
Ayush Kavad <ayushkavad@gmail.com>
2023-07-08 12:37:44 +0530

    compose file

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
-require('dotenv').config({ path: './config.env' });
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-const app = require('./app');
+const app = require('./app')
 
-const port = process.env.PORT || 3000;
+const DB = process.env.DATABASE_URL.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_ROOT_PASSWORD
+)
 
-console.log(process.env.HOST, process.env.PORT);
+const DBConnection = (DB) => {
+  return new Promise((resolve, reject) => {
+    mongoose.set('strictQuery', false)
+    mongoose
+      .connect(DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        resolve()
+      })
+      .catch((error) => {
+        reject(error)
+      })
+  })
+}
+
+DBConnection(DB)
+  .then(() => {
+    console.log(`DB connect successfully...`)
+  })
+  .catch((error) => {
+    console.log(`Something want wrong please try again later... ${error}`)
+  })
+
+const port = process.env.PORT || 3000
+
+console.log(process.env.HOST, process.env.PORT)
 
 app.listen(port, () => {
-  console.log(`Server running at http://${process.env.HOST}:${port}/`);
-});
+  console.log(`Server running at http://${process.env.HOST}:${port}/`)
+})
