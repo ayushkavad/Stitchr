1588b41978dfaae66540a006e218036505730fa6
Ayush Kavad <ayushkavad@gmail.com>
2023-07-10 12:02:29 +0530

    expires cookies

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -45,3 +45,14 @@ exports.login = async (req, res, next) => {
 
   createSendToken(user, 200, res)
 }
+
+exports.logout = (req, res, next) => {
+  // Set a cookie with a short expiration date to invalidate the JWT token
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  })
+
+  // Indicate success
+  res.status(200).json({ status: 'success' })
+}
--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { signup, login } = require('./../controllers/auth.controller')
+const { signup, login, logout } = require('./../controllers/auth.controller')
 
 const router = express.Router()
 
 router.post('/signup', signup)
 router.post('/login', login)
+router.post('/logout', logout)
 
 module.exports = router
