c068f54cc42dfccb048ac5d8a45d14d2c674584a
Ayush Kavad <ayushkavad@gmail.com>
2023-07-15 19:38:33 +0530

    refactor in errorHandler.js

--- src/controllers/errorHandlerController.js
+++ src/controllers/errorHandlerController.js
@@ -7,15 +7,12 @@ const handleCastErrorDB = (err) => {
 
 const handleDuplicateFieldsDB = (err) => {
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]
-  console.log(value)
-
   const message = `Duplicate field value: ${value}. Please use another value!`
   return new AppError(message, 400)
 }
 
 const handleValidationErrorDB = (err) => {
   const errors = Object.values(err.errors).map((el) => el.message)
-
   const message = `Invalid input data. ${errors.join('. ')}`
   return new AppError(message, 400)
 }
@@ -29,6 +26,7 @@ const handleJWTExpiredError = () =>
 const sendErrorDev = (err, req, res) => {
   // A) API
   if (req.originalUrl.startsWith('/api')) {
+    // A) Send error in development environment
     return res.status(err.statusCode).json({
       status: err.status,
       error: err,
@@ -36,13 +34,6 @@ const sendErrorDev = (err, req, res) => {
       stack: err.stack,
     })
   }
-
-  // B) RENDERED WEBSITE
-  console.error('ERROR 💥', err)
-  return res.status(err.statusCode).render('error', {
-    title: 'Something went wrong!',
-    msg: err.message,
-  })
 }
 
 const sendErrorProd = (err, req, res) => {
@@ -64,44 +55,14 @@ const sendErrorProd = (err, req, res) => {
       message: 'Something went very wrong!',
     })
   }
-
-  // B) RENDERED WEBSITE
-  // A) Operational, trusted error: send message to client
-  if (err.isOperational) {
-    console.log(err)
-    return res.status(err.statusCode).render('error', {
-      title: 'Something went wrong!',
-      msg: err.message,
-    })
-  }
-  // B) Programming or other unknown error: don't leak error details
-  // 1) Log error
-  console.error('ERROR 💥', err)
-  // 2) Send generic message
-  return res.status(err.statusCode).render('error', {
-    title: 'Something went wrong!',
-    msg: 'Please try again later.',
-  })
 }
 
-module.exports = (err, req, res, next) => {
-  // console.log(err.stack);
-
-  err.statusCode = err.statusCode || 500
-  err.status = err.status || 'error'
-
-  if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, req, res)
-  } else if (process.env.NODE_ENV === 'production') {
-    let error = { ...err }
-    error.message = err.message
-
-    if (error.name === 'CastError') error = handleCastErrorDB(error)
-    if (error.code === 11000) error = handleDuplicateFieldsDB(error)
-    if (error.name === 'ValidationError') error = handleValidationErrorDB(error)
-    if (error.name === 'JsonWebTokenError') error = handleJWTError()
-    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError()
-
-    sendErrorProd(error, req, res)
-  }
+module.exports = {
+  sendErrorDev,
+  sendErrorProd,
+  handleCastErrorDB,
+  handleDuplicateFieldsDB,
+  handleValidationErrorDB,
+  handleJWTError,
+  handleJWTExpiredError,
 }
---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,29 @@
+const {
+  sendErrorDev,
+  sendErrorProd,
+  handleCastErrorDB,
+  handleDuplicateFieldsDB,
+  handleValidationErrorDB,
+  handleJWTError,
+  handleJWTExpiredError,
+} = require('./../controllers/errorHandlerController')
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500
+  err.status = err.status || 'error'
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, req, res)
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err }
+    error.message = err.message
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error)
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error)
+    if (error.name === 'ValidationError') error = handleValidationErrorDB(error)
+    if (error.name === 'JsonWebTokenError') error = handleJWTError()
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError()
+
+    sendErrorProd(error, req, res)
+  }
+}
