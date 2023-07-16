aaab12295ec7133f5dafb4e7e7644282c4022372
Ayush Kavad <ayushkavad@gmail.com>
2023-07-16 18:04:11 +0530

    changes in file structure

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,3 @@
-const { promisify } = require('util')
 const jwt = require('jsonwebtoken')
 const catchAsync = require('../utils/catchAsync')
 const User = require('./../models/userModel')
@@ -42,44 +41,4 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError(`No user found with that email ID.`, 404))
   }
   createSendToken(user, 200, res)
-})
-
-exports.protect = catchAsync(async (req, res, next) => {
-  let token
-
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
-    token = req.headers.authorization.split(' ')[1]
-  }
-
-  if (!token) {
-    return next(
-      new AppError(`You are not login. Please login to get access!`, 401)
-    )
-  }
-
-  const decode = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
-
-  const currentUser = await User.findById(decode.id)
-
-  if (!currentUser) {
-    return next(
-      new AppError(`The user belong to this token doesn't exist!`, 401)
-    )
-  }
-
-  if (currentUser.changePasswordAfter(decode.iat)) {
-    return next(
-      new AppError(
-        `You recently changed your password. Please try again later!`,
-        401
-      )
-    )
-  }
-
-  req.user = currentUser
-
-  next()
 })
---
+++ src/middlewares/protect.js
@@ -0,0 +1,42 @@
+const { promisify } = require('util')
+const User = require('./../models/userModel')
+const AppError = require('./../utils/appError')
+
+exports.protect = catchAsync(async (req, res, next) => {
+  let token
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1]
+  }
+
+  if (!token) {
+    return next(
+      new AppError(`You are not login. Please login to get access!`, 401)
+    )
+  }
+
+  const decode = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
+
+  const currentUser = await User.findById(decode.id)
+
+  if (!currentUser) {
+    return next(
+      new AppError(`The user belong to this token doesn't exist!`, 401)
+    )
+  }
+
+  if (currentUser.changePasswordAfter(decode.iat)) {
+    return next(
+      new AppError(
+        `You recently changed your password. Please try again later!`,
+        401
+      )
+    )
+  }
+
+  req.user = currentUser
+  next()
+})
